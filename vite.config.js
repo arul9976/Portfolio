23785f1457a89588f008303f35f52cdbb0c4c139
Arulkumar <arunmicheal8@gmail.com>
2024-04-22 10:22:17 +0530

    v3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://arul9976.github.io/Portfolio/',
   server: {
     host: '0.0.0.0', // Or any desired host/IP
   },
