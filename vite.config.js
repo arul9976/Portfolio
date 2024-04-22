cfbebd9f9ed1c2d54accb77e9eb34fd482cea7d4
Arulkumar <arunmicheal8@gmail.com>
2024-04-22 10:32:22 +0530

    v4

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { ghPages } from 'vite-plugin-gh-pages'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://arul9976.github.io/Portfolio/',
+  plugins: [
+    react(),
+    ghPages(),
+  ],
+
+  base: '/Portfolio/',
   server: {
     host: '0.0.0.0', // Or any desired host/IP
   },
