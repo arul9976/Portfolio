e3b3dead2938ac17e341acd1d16ac7d181c4b11c
Arulkumar <arunmicheal8@gmail.com>
2024-04-22 14:42:20 +0530

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Portfolio'>
+    <BrowserRouter basename='/Portfolio/'>
       <ChakraProvider>
         <App />
       </ChakraProvider>
